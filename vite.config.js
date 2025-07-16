5477a6a57ceb18f2ad7664403815f6fd8b6cf26b
satyajeetch1991 <chavansatyajeet2004@gmail.com>
2025-07-16 19:01:30 +0530

    vite_config issue solve of preview

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: './', 
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('three') || id.includes('@react-three') || id.includes('maath')) {
-              return 'three-vendor';
-            }
-            if (id.includes('react')) {
-              return 'react-vendor';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1200,
-  },
 })
